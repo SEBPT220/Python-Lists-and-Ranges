f95e505a9870b346959ae0f7250f1a7c570d6801
Denis S Dujota <denisdujota@gmail.com>
2023-12-09 13:11:19 -0500

    finish lists

--- instructor/list-ranges.py
+++ instructor/list-ranges.py
@@ -59,3 +59,115 @@ print('***************************************************')
 
 for idx, color in enumerate(colors):
   print(idx, color)
+
+# LIST COMPREHENSIONS
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+squares = []
+for n in nums:
+  squares.append(n * n)
+
+print(nums)
+print(squares)
+
+print('***************************************************')
+squares2 = [n * n for n in nums]
+
+print(squares2)
+print(nums)
+print('***************************************************')
+
+even_squares = []
+for n in nums:
+  square = n * n
+  if square % 2 == 0:
+    even_squares.append(square)
+
+print(even_squares)
+
+print('***************************************************')
+ # I want 'n * n' for each 'n' in nums  if 'n * n' is even
+square_evens2 = [n * n for n in nums if n % 2 == 0]
+print(square_evens2)
+print('***************************************************')
+
+
+
+names = ('Bruce', 'Clark')
+clark = names[1]
+print(clark)
+
+# peter_idx = names.index('gfsefadsfsd') <--- not in index error
+# print(peter_idx)
+print('***************************************************')
+
+what, ever = names
+
+print(what)
+print(ever)
+
+
+print('***************************************************')
+obj = {"a":1, "b":2}
+
+# (k,v) tuyple, we actually are unpacking the tuple into two variables
+for dasda, jkdfsabhndsaf in obj.items():
+  print(dasda)
+  print(jkdfsabhndsaf)
+
+
+print('***************************************************')
+
+tup = (
+   (1,(5,6)),
+   (3,4)
+   )
+
+nums_string = ''
+tup2 = (
+  (1,2),
+  (3,4),
+  (5,6),
+  (7,8)
+)
+
+# 1,2,3,4,5,6,7,8
+for num1, num2 in tup2:
+  nums_string += str(num1)
+  nums_string += str(num2)
+
+print(nums_string)
+
+(num, (num2, num3)), second = tup
+print(num3)
+
+print('***************************************************')
+# RANGES
+
+# incremental range
+
+for even in range(2, 10, 2):
+  print(even)
+
+odds = tuple(range(1, 10, 2))
+print(odds)
+
+for num in range(5, 0, -1):
+ 	print(num)
+
+# Slicing Squence Types
+# Strings
+short_name = 'Alexandria'[0:4]
+
+print(short_name)
+
+
+# Lists
+sliced_colors = colors[:2]
+print(sliced_colors)
+
+# Tuples
+animals = ('cat', 'dog', 'mouse', 'bird', 'fish', 'lizard', 'snake', 'monkey')
+sliced_animals = animals[1:]
+
+print(sliced_animals)
